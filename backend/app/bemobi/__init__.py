589a583ccdd20a0d51521cfeab776dde66d60f5d
MKej88 <mads.kristensen2@gmail.com>
2026-08-17 10:34:42 +0200

    Phase 10: add Bemobi CVM news package

---
+++ backend/app/bemobi/__init__.py
@@ -0,0 +1,21 @@
+from app.bemobi.cvm_ipe import (
+    BEMOBI_CNPJ,
+    BEMOBI_CVM_CODE,
+    CVMIPERecord,
+    bemobi_cvm_news_status,
+    classify_cvm_ipe_record,
+    collect_bemobi_cvm_news,
+    list_bemobi_news,
+    parse_cvm_ipe_archive,
+)
+
+__all__ = [
+    "BEMOBI_CNPJ",
+    "BEMOBI_CVM_CODE",
+    "CVMIPERecord",
+    "bemobi_cvm_news_status",
+    "classify_cvm_ipe_record",
+    "collect_bemobi_cvm_news",
+    "list_bemobi_news",
+    "parse_cvm_ipe_archive",
+]
