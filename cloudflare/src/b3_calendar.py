23edbfecf853bf6a9a0e11ee564ef969060f1a7f
MKej88 <mads.kristensen2@gmail.com>
2026-08-17 23:47:35 +0200

    Correct Worker Easter calculation

--- cloudflare/src/b3_calendar.py
+++ cloudflare/src/b3_calendar.py
@@ -7,14 +7,14 @@ def _easter_sunday(year: int) -> date:
     """Gregorian Easter Sunday (Meeus/Jones/Butcher)."""
     a = year % 19
     b = year // 100
-    c = year % 19
+    c = year % 100
     d = b // 4
     e = b % 4
     f = (b + 8) // 25
     g = (b - f + 1) // 3
     h = (19 * a + b - d - g + 15) % 30
-    i = (year % 100) // 4
-    k = year % 4
+    i = c // 4
+    k = c % 4
     l = (32 + 2 * e + 2 * i - h - k) % 7
     m = (a + 11 * h + 22 * l) // 451
     month = (h + l - 7 * m + 114) // 31
