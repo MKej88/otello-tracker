1c6dd0dc0cb45a488cc3dde0cbfe12140c0b9d2a
MKej88 <mads.kristensen2@gmail.com>
2026-08-18 09:02:32 +0200

    Mount economic NAV panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import EconomicNavPanel from "./EconomicNavPanel";
 import "./styles.css";
 import "./prelive.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <EconomicNavPanel />
   </React.StrictMode>
 );
