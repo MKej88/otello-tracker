c15fff030c4cd500be450e4ce86546e70e494691
MKej88 <mads.kristensen2@gmail.com>
2026-08-19 22:33:57 +0200

    Eksponer aksjonær-workflow fra Worker-entrypoint

---
+++ cloudflare/src/worker.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from entry import Default, FullRefreshWorkflow, R2SnapshotDrillWorkflow
+from shareholder_snapshot_workflow import ShareholderSnapshotWorkflow
+
+__all__ = [
+    "Default",
+    "FullRefreshWorkflow",
+    "R2SnapshotDrillWorkflow",
+    "ShareholderSnapshotWorkflow",
+]
