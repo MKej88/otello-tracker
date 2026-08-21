38b8c82859224405c51b8e7ecae3569267bd9c92
MKej88 <mads.kristensen2@gmail.com>
2026-08-21 13:46:07 +0200

    Plasser kompatibilitetsdocstring før future-import

--- cloudflare/src/bemobi_web_refresh_v2.py
+++ cloudflare/src/bemobi_web_refresh_v2.py
@@ -1,10 +1,10 @@
-from __future__ import annotations
-
 """Bakoverkompatibel importsti for eldre Cloudflare Workflow-instansar.
 
 Ny produksjonskode skal bruke ``bemobi_web_refresh_runtime``. Denne filen beholdes
 midlertidig fordi durable Workflow-instansar kan gjenopptas mot en eldre modulsti.
 """
+
+from __future__ import annotations
 
 from bemobi_web_refresh_runtime import (
     _ensure_consensus_event,
