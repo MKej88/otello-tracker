0062aa11de71298d1f1aab646fbb5b8650e4e138
MKej88 <mads.kristensen2@gmail.com>
2026-08-24 18:41:27 +0200

    Aktiver ny investorside og fjern globale diagnosekort

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import DeferredDiagnostics from "./DeferredDiagnostics";
+import InvestorApp from "./InvestorApp";
 import { installDashboardBootstrapFetch } from "./dashboardBootstrapFetch";
 import "./styles.css";
 import "./prelive.css";
@@ -10,7 +9,6 @@ installDashboardBootstrapFetch();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <DeferredDiagnostics />
+    <InvestorApp />
   </React.StrictMode>
 );
