2288559aedf40b16b73b9dc9af5d627a95a1414e
MKej88 <mads.kristensen2@gmail.com>
2026-08-31 08:25:17 +0200

    Trim legacy Bemobi refresh shim

--- cloudflare/src/bemobi_web_refresh_v2.py
+++ cloudflare/src/bemobi_web_refresh_v2.py
@@ -6,20 +6,6 @@ midlertidig fordi durable Workflow-instansar kan gjenopptas mot en eldre modulst
 
 from __future__ import annotations
 
-from bemobi_web_refresh_runtime import (
-    _ensure_consensus_event,
-    _snapshot_payload,
-    _store_forward_snapshot,
-    parse_forward_consensus_html,
-    refresh_bemobi_web,
-    sync_marketscreener_consensus,
-)
+from bemobi_web_refresh_runtime import _ensure_consensus_event, refresh_bemobi_web
 
-__all__ = [
-    "_ensure_consensus_event",
-    "_snapshot_payload",
-    "_store_forward_snapshot",
-    "parse_forward_consensus_html",
-    "refresh_bemobi_web",
-    "sync_marketscreener_consensus",
-]
+__all__ = ["_ensure_consensus_event", "refresh_bemobi_web"]
