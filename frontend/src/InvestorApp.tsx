c4526b760d3403e8fd666eeca7ba584263e55bb3
MKej88 <mads.kristensen2@gmail.com>
2026-09-05 21:20:53 +0200

    Load grouped navigation styles

--- frontend/src/InvestorApp.tsx
+++ frontend/src/InvestorApp.tsx
@@ -6,6 +6,7 @@ import { discountHistoryUrl, investorPeriods } from "./investorPeriods";
 import { preloadJson, preloadNavPeriodBundle } from "./navigationDataPreload";
 import "./investor-v2.css";
 import "./otello-theme.css";
+import "./navigation-groups.css";
 
 const loadNavPage = () => import("./NavPageV2");
 const loadNavSensitivityPage = () => import("./NavSensitivityPage");
